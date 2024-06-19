368dcbe48c2ed772146e4e45d7d87344f55fc75e
mau2602 <machado.mauroc@gmail.com>
2024-06-19 15:02:27 -0400

    task 6 completion

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,28 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const doesExist = (username)=>{
+        let userswithsamename = users.filter((user)=>{
+          return user.username === username
+        });
+        if(userswithsamename.length > 0){
+          return true;
+        } else {
+          return false;
+        }
+      }
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -25,7 +45,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
     }
 })
  
-  
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     let author = req.params.author
